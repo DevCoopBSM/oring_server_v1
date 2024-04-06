d65a5e8b6b9a218c945ed7cbacb86cac0622984a
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-07 00:17:31 +0900

    SET: 프로젝트 세팅

---
+++ src/main/java/bsm/devcoop/oring_server/OringServerApplication.java
@@ -0,0 +1,13 @@
+package bsm.devcoop.oring_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OringServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OringServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/bsm/devcoop/oring_server/OringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bsm.devcoop.oring_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OringServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
